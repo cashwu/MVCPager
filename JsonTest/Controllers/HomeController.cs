37a36c6cb9a9eb9c65be83e38c9187e896892bd8
cashwu <cash@cashwu.com>
2014-03-01 23:24:55 +0800

    JSON Linq Test

--- JsonTest/Controllers/HomeController.cs
+++ JsonTest/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using JsonTest.Models;
 using Newtonsoft.Json;
 using System.Web.Script.Serialization;
 using JsonTest.Helper;
+using Newtonsoft.Json.Linq;
 
 namespace JsonTest.Controllers
 {
@@ -32,21 +33,48 @@ namespace JsonTest.Controllers
             return View();
         }
 
-        public ActionResult Product(int? id)
+        public ActionResult Json()
         {
-            if (!id.HasValue)
-            {
-                Dictionary<string, string> jo = new Dictionary<string, string>();
-                jo.Add("Msg", "please input product id");
-                return Content(JsonConvert.SerializeObject(jo), "application/json");
-            }
-            else
-            {
-                var product = db.Products.Single(a => a.ProductID == id.Value);
-                string jsonContent = new EFJavaScriptSerializer().Serialize(product);
+            return View();
+        }
 
-                return new ContentResult { Content = jsonContent, ContentType = "application/json" };
+        public ActionResult Product()
+        {
+            var data = db.Products;
+
+            JArray ja = new JArray();
+            foreach (var item in data)
+            {
+                JObject jo = new JObject();
+                jo.Add("ProductID", item.ProductID);
+                jo.Add("ProductName", item.ProductName);
+                jo.Add("CategoryID", item.CategoryID);
+                jo.Add("CategoryName", item.Category != null ? item.Category.CategoryName : string.Empty);
+                jo.Add("SupplierID", item.SupplierID);
+                jo.Add("CompanyName", item.Supplier != null ? item.Supplier.CompanyName : string.Empty);
+                ja.Add(jo);
             }
+
+            return Content(JsonConvert.SerializeObject(ja), "application/json");
+        }
+
+        public ActionResult Order()
+        {
+            var data = db.Orders;
+            JArray ja = new JArray();
+            foreach (var item in data)
+            {
+                JObject jo = new JObject();
+                jo.Add("OrderID", item.OrderID);
+                jo.Add("CustomerID", item.CustomerID);
+                jo.Add("EmployeeID", item.EmployeeID);
+                jo.Add("OrderDate", item.OrderDate);
+                jo.Add("ShippedDate", item.ShippedDate);
+                jo.Add("ShipCounty", item.ShipCountry);
+                ja.Add(jo);
+            }
+
+            return Content(JsonConvert.SerializeObject(ja), "application/json");
         }
     }
 }
---
+++ OracleTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OracleTest.Startup))]
+namespace OracleTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
