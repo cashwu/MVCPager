b06ce5f00fbc8270f1a6608d5310f904e50311b2
cashwu <cash@cashwu.com>
2014-03-02 23:34:06 +0800

    Image Area Select Test

---
+++ ImageAreaTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImageAreaTest.MvcApplication" Language="C#" %>
---
+++ ImageAreaTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImageAreaTest.Startup))]
+namespace ImageAreaTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
