8d8f3f7af9ee00c884e89ac3751aa2ced631e3e3
cashwu <cash@cashwu.com>
2014-03-09 23:37:29 +0800

    FormsAuthentication

---
+++ FormsAuthenticationTest/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using FormsAuthenticationTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormsAuthenticationTest.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            //ReturnUrl字串是使用者在未登入情況下要求的的Url
+            LoginViewModel model = new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            FormsAuthentication.RedirectFromLoginPage(model.Account, false);
+
+            return Redirect(FormsAuthentication.GetRedirectUrl(model.Account, false));
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login", "Home");
+        }
+
+        public ActionResult Index()
+        {
+            string account = User.Identity.Name;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FormsAuthenticationTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FormsAuthenticationTest.MvcApplication" Language="C#" %>
