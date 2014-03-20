34e03760e5a66e122152f7ff10679f567b7fb5ea
cashwu <cash@cashwu.com>
2014-03-20 17:44:40 +0800

    template

--- FileUploadTest/Models/HomeViewModel.cs
+++ FileUploadTest/Models/HomeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,24 @@ namespace FileUploadTest.Models
 {
     public class HomeViewModel
     {
-        [UIHint("FineUploader")]
+        [DataType(DataType.Upload)]
         public string UploadUrl { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayName("選擇日期")]
+        [Required]
+        public DateTime UploadDate { get; set; }
+
+        [Required()]
+        public string Date { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "必填")]
+        public string Phone { get; set; }
+
+        [UIHint("MemberID")]
+        [Required(ErrorMessage = "必填")]
+        public string MemberID { get; set; }
     }
 }
